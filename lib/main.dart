349299b00feed2d5f352e64a7080cfa9a6ddfbb3
Daniel Reis <daniel.reis@lslog.com.br>
2020-06-16 16:47:09 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Image.asset('images/bytebank_logo.png'),
+          ],
+        ),
+      ),
+    );
+  }
+}
